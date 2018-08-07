49fe3270a2aa21a3223caf6fdb6f17157b925d37
Vlad Nikonov <vlad.nikonov@offsiteteam.com>
2018-08-07 13:12:20 +0300

    Lab 1: Added simple form for Selling book

--- Lab1/MetanitGuidModded/Controllers/HomeController.cs
+++ Lab1/MetanitGuidModded/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MetanitGuidModded.Models;
+using MetanitGuidModded.Models.BookStore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,28 @@ namespace MetanitGuidModded.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            var books = db.Books;
+            ViewBag.Books = books;
             return View();
+        }
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // Add purchase info into DB
+            db.Purchases.Add(purchase);
+            // Save changes in DB
+            db.SaveChanges();
+            return purchase.Person + ", thanks for your purchase!";
         }
 
         public ActionResult About()
