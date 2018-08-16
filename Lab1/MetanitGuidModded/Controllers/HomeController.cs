183f5862156799c30e5d6e12caafe85868212c89
Vlad Nikonov <vlad.nikonov@offsiteteam.com>
2018-08-16 15:39:45 +0300

    Created some Controller methods, dome till module 12 (Status Codes generation)

--- Lab1/MetanitGuidModded/Controllers/HomeController.cs
+++ Lab1/MetanitGuidModded/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MetanitGuidModded.Models;
 using MetanitGuidModded.Models.BookStore;
+using MetanitGuidModded.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,18 @@ namespace MetanitGuidModded.Controllers
     public class HomeController : Controller
     {
         BookContext db = new BookContext();
-        public ActionResult Index()
+        public ViewResult Index()
         {
             var books = db.Books;
             ViewBag.Books = books;
+
+            ViewData["Head"] = "Hello world!";
+            //== ViewBag.Head = "Hello world"; and initialize in Index.cshtml using @ViewBag.Head
+
+            ViewBag.Fruit = new List<string>
+            {
+                "apples", "oranges", "bananas"
+            };
             return View();
         }
         [HttpGet]
@@ -34,6 +43,35 @@ namespace MetanitGuidModded.Controllers
             return purchase.Person + ", thanks for your purchase!";
         }
 
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+
+            double s = a * h / 2.0;
+            return "<h2>Площадь треугольника с основанием " + a +
+                    " и высотой " + h + " равна " + s + "</h2>";
+        }
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ContentResult PostBook(string title, string author)
+        {
+            return Content("Book \"" + title + "\" writen by " + author);
+        }
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2>Hello World!</h2>");
+        }
+        public ActionResult GetImage()
+        {
+            string path = "../Content/Images/pizza.png";
+            return new ImageResult(path);
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -46,6 +84,15 @@ namespace MetanitGuidModded.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult GetVoid(int id)
+        {
+            if(id>5)
+            {
+                return Redirect("/Home/Contact");
+            }
+            return View("About");
+            
         }
     }
 }
---
+++ Lab1/MetanitGuidModded/Util/HtmlResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetanitGuidModded.Util
+{
+    public class HtmlResult : ActionResult
+    {
+        private string htmlCode;
+
+        public HtmlResult(string html)
+        {
+            htmlCode = html;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Dashboard</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += htmlCode;
+            fullHtmlCode += "</body></html>";
+            context.HttpContext.Response.Write(fullHtmlCode);
+        }
+    }
+}
